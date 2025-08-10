e7d591d65109498834f5a256e2738e0fd6d0225e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-08-10 02:18:17 +0100

    build a CRUD Rest Api

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware
+
+app.use(express.json());
+app.use(cors);
+
+// Routes
+
+// Error handling middleware
+
+// Server running 
+app.listen(port, () => {
+    console.log(`Server is running on http:localhost:${port}`);
+});
