1428d7c47c16886d51b1043e0be6cfde3e60ac82
Isaac Alexander <Isaacalexander81@gmail.com>
2025-08-12 02:03:42 +0100

    src/routes/

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/user", createUser);
+router.get("/user", getAllUser);
+router.get("/user/:id", getUserById);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
+
+
+export default router;
