a531e8e6ac53b4636e103de4db0a36513b46bf41
Isaac Alexander <Isaacalexander81@gmail.com>
2025-09-27 02:01:35 +0100

    src/index.js

--- src/index.js
+++ src/index.js
@@ -34,5 +34,5 @@ app.get("/", async (req, res) => {
 
 // Server running 
 app.listen(port, () => {
-    console.log(`Server is running on http:localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
